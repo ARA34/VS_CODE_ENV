acd7234940486b3be9ac6c6c0c041262c4b30374
Alex ReyesAranda <areyesar@uci.edu>
2024-01-10 19:48:26 -0800

    removed unecessary files

--- proj0.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-output = "+-+\n| |\n+-+"
-for i in range(n):
-    if i != n-1:
-        # inside blocks
-        output += "-+\n" + "  "*(i+1) + "| |\n" + "  "*(i+1) + "+-+" 
-
-print(output)
