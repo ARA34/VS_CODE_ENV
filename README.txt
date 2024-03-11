ce81c1e12e3968a0ff8f1bf0d7ef6af35fefc4fb
Alex ReyesAranda <areyesar@uci.edu>
2024-03-11 15:55:55 -0700

    refractor

--- README.txt
+++ README.txt
@@ -5,3 +5,14 @@ You can run the program by typing "python3 a0.py" in the terminal and then input
 Contact:
 Alex Reyes Aranda
 areyesar@uci.edu
+
+
+In the refractor part of this assignment, I created a new class called Diagram.
+
+In the Diagram class I introduce attributes iterations, and output.
+The iteratrions attribute is original set to the lowest positive integer but can be changed
+with the class function called "ask_input". The ask_input functions takes input from the user
+converts to integer and assigns it to the iterations attribute.
+This iterations attribute can then be used in the class function called blockdiag which creates
+the block diagram and stores it in the class's output attribute. 
+The ouput attribute is then printed out by the print_output function.
--- a0.py
+++ a0.py
@@ -1,21 +1,56 @@
-def blockdiag(n):
-    # n is a positive integer
-    output = ""
-    top = "+-+"
-    middle = "| |"
-    bottom = "+-+-+"
-    drop = "\n"
-    tab = " "
-    output += top + drop
-    output += middle + drop
-    for i in range(1,n):
-        output += bottom + drop + (2*i) * tab
-        output += middle + drop
-        if i != n-1:
-            output += (2*i) * tab
-        else:
-            output += (2*i) * tab + top
-    return output
+"""
+This module allows the user to create a digaram and interect with its functions and attributes
+"""
 
-user_in = input()
-print(blockdiag(int(user_in)))
+class Diagram():
+    """
+    This is the class to create diagrams
+    """
+    def __init__(self):
+        self.iterations = 1
+        self.output = None
+
+    def ask_input(self) -> None:
+        """
+        Asks for input from user
+        """
+        usr_input = int(input("Enter a positive integer: "))
+        self.iterations = usr_input
+
+    def print_output(self):
+        """
+        Prints the output attribute
+        """
+        if self.output is None:
+            print("There is nothing to output")
+        else:
+            print(self.output)
+
+    def blockdiag(self) -> None:
+        """
+        Sets the output attribute to the diagram
+        """
+        n = self.iterations
+        # n is a positive integer
+        output = ""
+        top = "+-+"
+        middle = "| |"
+        bottom = "+-+-+"
+        drop = "\n"
+        tab = " "
+        output += top + drop
+        output += middle + drop
+        for i in range(1, n):
+            output += bottom + drop + (2*i) * tab
+            output += middle + drop
+            if i != n-1:
+                output += (2*i) * tab
+            else:
+                output += (2*i) * tab + top
+        self.output = output
+
+
+c1 = Diagram()
+c1.ask_input()
+c1.blockdiag()
+c1.print_output()
